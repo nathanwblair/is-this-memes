67629684748f970be20d6276eb1ff7a0aeb28694
Sean Volders <sean.volders@ad.aie.edu>
2016-05-16 13:40:20 +1000

    Android project settings

---
+++ Assets/InputComponent.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class InputComponent : MonoBehaviour {
+
+    Vector2 touchStart;
+    Vector2 touchCurrent;
+
+	// Use this for initialization
+	void Start ()
+    {
+	    
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	    if (Input.touchCount > 0)
+        {
+
+        }
+        
+	}
+}
