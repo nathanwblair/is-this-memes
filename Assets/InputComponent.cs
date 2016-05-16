ee5c1d2b3cf8eca53f0b1d32c04571ea73cca83c
Sean Volders <sean.volders@ad.aie.edu>
2016-05-16 13:41:29 +1000

    Input stuff

--- Assets/InputComponent.cs
+++ Assets/InputComponent.cs
@@ -1,23 +1,31 @@
 ﻿using UnityEngine;
 using System.Collections;
 
-public class InputComponent : MonoBehaviour {
-
+public class InputComponent : MonoBehaviour
+{
     Vector2 touchStart;
     Vector2 touchCurrent;
 
-	// Use this for initialization
-	void Start ()
+    // Use this for initialization
+    void Start()
     {
-	    
-	}
-	
-	// Update is called once per frame
-	void Update () {
-	    if (Input.touchCount > 0)
-        {
+    }
 
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.touchCount > 0)
+        {
+            if (Input.GetTouch(0).phase == TouchPhase.Began)
+            {
+                touchStart = Input.GetTouch(0).position;
+            }
+            if (Input.GetTouch(0).phase == TouchPhase.Moved)
+            {
+                touchCurrent = Input.GetTouch(0).position;
+            }
+
+            Debug.DrawLine(touchStart, touchCurrent, Color.red);
         }
-        
-	}
+    }
 }
